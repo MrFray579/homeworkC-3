aaddc9f8aa5a18cbbb5b8a3fce4be07ec76f7ab8
Fray <kozar20000@mail.ru>
2022-07-01 15:41:57 +0300

    degree

---
+++ aTask2/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] calculad (int a)
+{
+    int[] num = new int[a];
+    int i = 1;
+
+    while (i <= a)
+    {
+        num[i-1] = i * i * i;
+        Console.Write(num[i-1] + ",");
+        i++;
+    }
+    return num;
+}
+
+int[] result = calculad(5);
+
+
+/*Console.WriteLine("Введите число:");
+int a  = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[a];
+int i = 1;
+
+while (i <= a)
+{
+    array[i - 1] = i * i * i;
+    Console.Write(array[i -1] + ",");
+    i++;
+}*/
